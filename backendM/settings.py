cb8259e25b148effa915ca52e9cb22435fa3eedd
Rafael Ramírez <rrramirezb251@gmail.com>
2021-10-08 15:17:36 -0500

    employee added

--- backendM/settings.py
+++ backendM/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backendM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Inventory',
+        'USER': 'root',
+        'PASSWORD': '1465Rafael*',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
